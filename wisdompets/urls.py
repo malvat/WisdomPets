8bc9f134f342acc914eb19464fa23dbc3f84fcb4
malvat <malvatanim@gmail.com>
2020-06-23 23:05:08 -0400

    use template and make some proper template

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -20,5 +20,5 @@ from adoptions import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
-    path('adoptions/<int:pet_id>/', views.pet_detail, name="pet detail")
+    path('adoptions/<int:pet_id>/', views.pet_detail, name="pet_detail")
 ]
