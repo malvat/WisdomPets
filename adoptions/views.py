6ede30bce3b0d8c0f3b9d8ab1d10b275637c9e6f
malvat <malvatanim@gmail.com>
2020-06-23 22:01:14 -0400

    add url patterns for home and pet_detail

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Pet
 
-# Create your views here.
+def home(request):
+    pet = Pet.objects.all()
+    return render(request, 'home.html', {
+        "pets": pet,
+    })
+
+def pet_detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id = pet_id)
+    except: 
+        raise Http404('pet object not found')
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
