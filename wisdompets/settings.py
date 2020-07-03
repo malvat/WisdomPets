d3f6ceb3903d30412045de2f50873a9b495544bd
malvat <malvatanim@gmail.com>
2020-07-03 02:49:08 -0400

    deploy web application

--- wisdompets/settings.py
+++ wisdompets/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+# to work with heroku
+django_heroku_settings(local())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
